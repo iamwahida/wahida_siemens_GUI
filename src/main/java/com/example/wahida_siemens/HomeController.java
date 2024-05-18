f3ab0cb7d5901c4c0d11e77ae03055416439ac6d
iamwahida <nemati.w@yahoo.com>
2024-05-18 12:55:44 +0200

    exercise done

--- src/main/java/com/example/wahida_siemens/HomeController.java
+++ src/main/java/com/example/wahida_siemens/HomeController.java
@@ -2,13 +2,27 @@ package com.example.wahida_siemens;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+public class HomeController {
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField input;
+
+    @FXML
+    private Label output;
+
+    @FXML
+    protected void reverseBtnClick() {
+        String str = input.getText();
+
+        if (str.isEmpty()) {
+            output.setText("Die Eingabe ist leer. Du kannst es nochmal versuchen :)");
+        } else {
+            StringBuilder reverseStr = new StringBuilder(str);
+            String reversed_text = reverseStr.reverse().toString();
+
+            output.setText("\n" + reversed_text);
+        }
     }
 }
